d86ea6efdddfbb8d3fc0277e33e124e034fb3556
tsammereyer <tobias418@gmail.com>
2018-05-22 14:19:44 +0200

    added register.php

---
+++ views/register.php
@@ -0,0 +1,51 @@
+<?php
+
+use Slacklight\AuthenticationManager;
+use Slacklight\Util;
+
+if (AuthenticationManager::isAuthenticated()) {
+    Util::redirect("index.php");
+}
+$userName = isset($_REQUEST['userName']) ? $_REQUEST['userName'] : null;
+?>
+
+<?php
+require_once('views/partials/header.php');
+?>
+
+
+    <div class="page-header">
+        <h2>Register</h2>
+    </div>
+
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            Please fill out the form below:
+        </div>
+        <div class="panel-body">
+
+            <form class="form-horizontal" method="post" action="<?php echo Util::action(Slacklight\Controller::ACTION_REGISTER, array('view' => $view)); ?>">
+                <div class="form-group">
+                    <label for="inputName" class="col-sm-2 control-label">User name:</label>
+                    <div class="col-sm-6">
+                        <input type="text" class="form-control" id="inputName" name="<?php print Slacklight\Controller::USER_NAME; ?>" placeholder="username" value="<?php echo htmlentities($userName); ?>">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="inputPassword" class="col-sm-2 control-label">Password</label>
+                    <div class="col-sm-6">
+                        <input type="password" class="form-control" id="inputPassword" name="<?php print Slacklight\Controller::USER_PASSWORD; ?>" placeholder="password">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-6">
+                        <button type="submit" class="btn btn-default">Register</button>
+                    </div>
+                </div>
+            </form>
+            <a href="<?php echo $_SERVER['PHP_SELF']; ?>?view=login" > you have already an account? -> Login</a>
+        </div>
+    </div>
+
+<?php
+require_once('views/partials/footer.php');
